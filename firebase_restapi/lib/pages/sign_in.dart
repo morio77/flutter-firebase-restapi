3bc9c974662b7ad06d94d7349e32113fdd55534f
morio77 <howtostudyy77@gmail.com>
2021-04-17 16:25:02 +0900

    ログイン機能のプロトを実装

--- firebase_restapi/lib/pages/sign_in.dart
+++ firebase_restapi/lib/pages/sign_in.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth_rest/firebase_auth_rest.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -20,35 +23,34 @@ class _SignInPageState extends State<SignInPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: emailTextEditingController,
-              enabled: true,
-              maxLength: 30,
-              style: TextStyle(color: Colors.black),
-              obscureText: false,
-              maxLines:1 ,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.mail),
-                hintText: 'メールアドレスを入力してください',
-                labelText: 'メールアドレス *',
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                controller: emailTextEditingController,
+                maxLength: 30,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.mail),
+                  hintText: 'メールアドレスを入力してください',
+                  labelText: 'メールアドレス *',
+                ),
               ),
             ),
-            TextField(
-              controller: passwordTextEditingController,
-              enabled: true,
-              maxLength: 30,
-              style: TextStyle(color: Colors.black),
-              obscureText: true,
-              maxLines:1 ,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.lock),
-                hintText: 'パスワードを入力してください',
-                labelText: 'パスワード *',
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                controller: passwordTextEditingController,
+                maxLength: 15,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: 'パスワードを入力してください',
+                  labelText: 'パスワード *',
+                ),
               ),
             ),
             ElevatedButton(
               onPressed: () => SignInWithPassword(),
-              child: Text('ログイン'),
+              child: Text('登録する'),
             )
           ],
         ),
@@ -58,6 +60,21 @@ class _SignInPageState extends State<SignInPage> {
 
   /// E-mail/Passでログイン
   Future<void> SignInWithPassword() async {
+    final client = Client();
+    final apiKey = env['APIKEY'];
+    try {
+      final fbAuth = FirebaseAuth(client, apiKey, 'ja-JP');
+      final account = await fbAuth.signInWithPassword(emailTextEditingController.text, passwordTextEditingController.text);
 
+      print(await account.getDetails());
+
+      Navigator.pop(context);
+
+    } catch (e) {
+      print(e);
+    } finally {
+      print('Clientをクローズします。');
+      client.close();
+    }
   }
 }
